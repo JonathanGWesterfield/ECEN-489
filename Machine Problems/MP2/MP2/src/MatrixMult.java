f993299141002d54f9b80eb754d907206de492c6
Jonathan G. Westerfield <jgwesterfield@gmail.com>
2019-01-21 15:22:58 -0600

    Finished MP2

--- Machine Problems/MP2/MP2/src/MatrixMult.java
+++ Machine Problems/MP2/MP2/src/MatrixMult.java
@@ -1,24 +1,349 @@
+import java.lang.reflect.Array;
+import java.rmi.activation.ActivationGroup_Stub;
 import java.util.*;
 
 public class MatrixMult
 {
     public static void main(String[] args)
     {
-        ArrayList<ArrayList<Integer>> mat1 = populateMatrix();
-        ArrayList<ArrayList<Integer>> mat2 = populateMatrix();
+        ArrayList<ArrayList<Integer>> mat1;
+        ArrayList<ArrayList<Integer>> mat2;
+        ArrayList<ArrayList<Integer>> resultMat;
 
-        printMatrix(mat1);
-        printMatrix(mat2);
+        boolean runSmallMatrix = getSizeChoice();
+        boolean runMultThread = getThreadChoice();
+        if(runSmallMatrix)
+        {
+            mat1 = populateMiniMatrix();
+            mat2 = populateMiniMatrix();
 
+            System.out.println("Matrix 1: ");
+            printMatrix(mat1);
+
+            System.out.println("Matrix 2: ");
+            printMatrix(mat2);
+
+            mat2 = rotateMatrix(mat2);
+            System.out.println("Rotated Matrix 2: ");
+            printMatrix(mat2);
+
+            // System.out.println(runMultThread);
+            if(runMultThread)
+            {
+                System.out.println("Multithreaded Matrix Operations");
+                resultMat = miniMatrixOps(mat1, mat2);
+            }
+            else
+            {
+                System.out.println("Single Threaded Matrix Operations");
+                resultMat = singleThreadMatrixOps(mat1, mat2);
+            }
+        }
+        else
+        {
+            mat1 = populateMatrix();
+            mat2 = populateMatrix();
+
+            System.out.println("Matrix 1: ");
+            printMatrix(mat1);
+
+            System.out.println("Matrix 2: ");
+            printMatrix(mat2);
+
+            mat2 = rotateMatrix(mat2);
+            System.out.println("Rotated Matrix 2: ");
+            printMatrix(mat2);
+
+            // System.out.println(runMultThread);
+            if(runMultThread)
+            {
+                ArrayList<ArrayList<ArrayList<Integer>>> argMatrix = argMatrixI(mat1);
+                print3DMatrix(argMatrix);
+
+                System.out.println("Multithreaded Matrix Operations");
+                resultMat = matrixOps(argMatrix, mat2);
+            }
+            else
+            {
+                System.out.println("Single Threaded Matrix Operations");
+                resultMat = singleThreadMatrixOps(mat1, mat2);
+            }
+        }
+
+        System.out.println("Result Matrix: ");
+        printMatrix(resultMat);
+
+        // TODO: Used 3d matrix to send corresponding rows to the threads
+
+    }
+
+    public static boolean getSizeChoice()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Populate Miniature Matrix? (y/n)");
+        String choice = keyboard.next();
+
+        if(choice.compareToIgnoreCase("y") == 0)
+        {
+            System.out.println("Using Smaller Matrix!!!");
+            return true;
+        }
+        else if(choice.compareToIgnoreCase("n") == 0)
+        {
+            System.out.println("Using Normal 20x20 Matrix");
+        }
+        else
+        {
+            System.out.println("Choice is not a listed option\nUsing Normal 20x20 Matrix\n");
+        }
+        return false;
+    }
+
+    public static boolean getThreadChoice()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Run Matrix Operations Multithreaded? (y/n)");
+        String choice = keyboard.next();
+
+        if(choice.compareToIgnoreCase("y") == 0)
+        {
+            System.out.println("Running Matrix Operations Multithreaded!!!");
+            return true;
+        }
+        else if(choice.compareToIgnoreCase("n") == 0)
+        {
+            System.out.println("Running Matrix Operations Single Threaded");
+        }
+        else
+        {
+            System.out.println("Choice is not a listed option\nRunning Matrix Operations Single Threaded\n");
+        }
+        return false;
+    }
+
+    public static ArrayList<ArrayList<Integer>> singleThreadMatrixOps(ArrayList<ArrayList<Integer>> mat1,
+                                                                      ArrayList<ArrayList<Integer>> mat2)
+    {
+        try
+        {
+            MatrixOps matOps = new MatrixOps(mat1, mat2);
+
+            matOps.start();
+
+            Thread.sleep(2000);
+
+            return matOps.getResultMat();
+        }
+        catch (Exception e)
+        {
+            System.out.println("ERROR!!! \n" + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ArrayList<ArrayList<Integer>> miniMatrixOps(ArrayList<ArrayList<Integer>> mat1,
+                                                              ArrayList<ArrayList<Integer>> mat2)
+    {
+        ArrayList<ArrayList<Integer>> argMat1 = new ArrayList<ArrayList<Integer>>();
+        argMat1.add(mat1.get(0));
+        System.out.println("First Arg: ");
+        printMatrix(argMat1);
+
+        ArrayList<ArrayList<Integer>> argMat2 = new ArrayList<ArrayList<Integer>>();
+        argMat2.add(mat1.get(1));
+        System.out.println("Second Arg: ");
+        printMatrix(argMat2);
+
+        ArrayList<ArrayList<Integer>> argMat3 = new ArrayList<ArrayList<Integer>>();
+        argMat3.add(mat1.get(2));
+        System.out.println("Third Arg: ");
+        printMatrix(argMat3);
+
+        try
+        {
+            MatrixOps matrixOps1 = new MatrixOps(argMat1, mat2);
+            MatrixOps matrixOps2 = new MatrixOps(argMat2, mat2);
+            MatrixOps matrixOps3 = new MatrixOps(argMat3, mat2);
+
+            matrixOps1.start();
+            matrixOps2.start();
+            matrixOps3.start();
+
+            Thread.sleep(1000);
+
+            return miniStitchTogether(matrixOps1.getResultMat(), matrixOps2.getResultMat(), matrixOps3.getResultMat());
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("ERROR: " + e.getMessage() + "\n");
+        }
+
+        return null;
+    }
+
+
+    public static ArrayList<ArrayList<Integer>> matrixOps(ArrayList<ArrayList<ArrayList<Integer>>> mat1,
+                                                          ArrayList<ArrayList<Integer>> mat2)
+    {
+        ArrayList<MatrixOps> matOps = new ArrayList<>();
+
+        try
+        {
+            // Create threads
+            for(int i = 0; i < 5; i++)
+            {
+                matOps.add(new MatrixOps(mat1.get(i), mat2));
+            }
+
+            // Start Threads
+            for(int i = 0; i < 5; i++)
+            {
+                matOps.get(i).start();
+            }
+
+            Thread.sleep(1000);
+
+            return stitchTogether(matOps.get(0).getResultMat(), matOps.get(1).getResultMat(), matOps.get(2).getResultMat(),
+                    matOps.get(3).getResultMat(), matOps.get(4).getResultMat());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("ERROR: " + e.getMessage() + "\n");
+        }
+
+        return null;
+    }
+
+    public static ArrayList<ArrayList<Integer>> miniStitchTogether(ArrayList<ArrayList<Integer>> mat1, ArrayList<ArrayList<Integer>> mat2,
+                                                               ArrayList<ArrayList<Integer>> mat3)
+    {
+        ArrayList<ArrayList<Integer>> newMat = new ArrayList<ArrayList<Integer>>();
+
+
+        for(int i = 0; i < mat1.size(); i++)
+        {
+            newMat.add(mat1.get(i));
+        }
+
+        for(int i = 0; i < mat2.size(); i++)
+        {
+            newMat.add(mat2.get(i));
+        }
+
+        for(int i = 0; i < mat3.size(); i++)
+        {
+            newMat.add(mat3.get(i));
+        }
+
+        return newMat;
+    }
+
+    // Stitches the returned 4x20 matrices back together to make a 20x20 matrix again
+    public static ArrayList<ArrayList<Integer>> stitchTogether(ArrayList<ArrayList<Integer>> mat1, ArrayList<ArrayList<Integer>> mat2,
+                                                        ArrayList<ArrayList<Integer>> mat3, ArrayList<ArrayList<Integer>> mat4,
+                                                        ArrayList<ArrayList<Integer>> mat5)
+    {
+        ArrayList<ArrayList<Integer>> newMat = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i < mat1.size(); i++)
+        {
+            newMat.add(mat1.get(i));
+        }
+
+        for(int i = 0; i < mat2.size(); i++)
+        {
+            newMat.add(mat2.get(i));
+        }
+
+        for(int i = 0; i < mat3.size(); i++)
+        {
+            newMat.add(mat3.get(i));
+        }
+
+        for(int i = 0; i < mat4.size(); i++)
+        {
+            newMat.add(mat4.get(i));
+        }
+
+        for(int i = 0; i < mat5.size(); i++)
+        {
+            newMat.add(mat5.get(i));
+        }
+
+        return newMat;
+    }
+
+    public static ArrayList<ArrayList<ArrayList<Integer>>> argMatrixI(ArrayList<ArrayList<Integer>> mat)
+    {
+        ArrayList<ArrayList<ArrayList<Integer>>> argMatrix = new ArrayList<ArrayList<ArrayList<Integer>>>();
+        ArrayList<ArrayList<Integer>> newRow;
+
+        int index = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            newRow = new ArrayList<ArrayList<Integer>>();
+
+            for (int j = 0; j < 4; j++)
+            {
+                newRow.add(mat.get(index));
+                index++;
+            }
+
+            argMatrix.add(newRow);
+        }
+
+        return argMatrix;
+    }
+
+    public static void print3DMatrix(ArrayList<ArrayList<ArrayList<Integer>>> argMatrix)
+    {
+        System.out.println("Printing 3D Arg matrix: ");
+        for(int i = 0; i < argMatrix.size(); i++)
+        {
+            for(int j = 0; j < argMatrix.get(i).size(); j++)
+            {
+                for(int k = 0; k < argMatrix.get(i).get(j).size(); k++)
+                {
+                    System.out.print("Matrix: " + i + " Row " + j+ " size: " + argMatrix.get(i).get(j).get(k) + " || ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<ArrayList<Integer>> populateMiniMatrix()
+    {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> inner;
+
+        for(int i = 0; i < 3; i++)
+        {
+            inner = new ArrayList<>();
+
+            for(int j = 0; j < 3; j++)
+            {
+                inner.add(genRandNum());
+            }
+            matrix.add(inner);
+        }
+
+        return matrix;
     }
 
     public static ArrayList<ArrayList<Integer>> populateMatrix()
     {
         ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> inner = new ArrayList<>();
+        ArrayList<Integer> inner;
 
         for(int i = 0; i < 20; i++)
         {
+            inner = new ArrayList<>();
+
             for(int j = 0; j < 20; j++)
             {
                 inner.add(genRandNum());
@@ -27,6 +352,26 @@ public class MatrixMult
         }
 
         return matrix;
+    }
+
+    public static ArrayList<ArrayList<Integer>> rotateMatrix(ArrayList<ArrayList<Integer>> mat)
+    {
+        ArrayList<ArrayList<Integer>> newMat = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> newRow;
+
+        for(int i = 0; i < mat.size(); i++)
+        {
+            newRow = new ArrayList<Integer>();
+
+            for(int j = 0; j < mat.get(i).size(); j++)
+            {
+                newRow.add(mat.get(j).get(i));
+            }
+
+            newMat.add(newRow);
+        }
+
+        return newMat;
     }
 
     public static void printMatrix(ArrayList<ArrayList<Integer>> matrix)
@@ -47,6 +392,6 @@ public class MatrixMult
     public static int genRandNum()
     {
         // 100 is the maximum and the 1 is our minimum.
-        return (int)(Math.random() * 100 + 1);
+        return (int)(Math.random() * 5 + 1);
     }
 }
--- Machine Problems/MP2/MP2/src/MatrixOps.java
+++ Machine Problems/MP2/MP2/src/MatrixOps.java
@@ -7,27 +7,64 @@ public class MatrixOps implements Runnable
     private ArrayList<ArrayList<Integer>> matrixJ;
     private ArrayList<ArrayList<Integer>> resultMat;
 
+    private Thread t;
+
     public MatrixOps(ArrayList<ArrayList<Integer>> rowI, ArrayList<ArrayList<Integer>> matrixJ)
     {
+        System.out.println("Creating Thread: " + Thread.currentThread() + "\n");
         this.rowI = rowI;
         this.matrixJ = matrixJ;
     }
 
-    public void run()
+    public void start ()
     {
-        resultMat = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> resultRow = new ArrayList<>();
-        for(int i = 0; i < this.rowI.size(); i++)
-        {
-            for(int j = 0; j < this.rowI.get(i).size(); j++)
-            {
-                // do row column operations
-            }
+        System.out.println("Starting Thread: ");
+        if (t == null) {
+            t = new Thread (this);
+            t.start ();
         }
     }
 
+    public ArrayList<ArrayList<Integer>> getResultMat()
+    {
+        return this.resultMat;
+    }
+
+    public void run()
+    {
+
+        resultMat = rowOps();
+    }
+
+    private ArrayList<ArrayList<Integer>> rowOps()
+    {
+        ArrayList<ArrayList<Integer>> newMat = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> newRow;
+        for(int i = 0; i < this.rowI.size(); i++)
+        {
+            newRow = new ArrayList<Integer>();
+
+            for(int j = 0; j < matrixJ.size(); j++)
+            {
+                newRow.add(rowColMult(this.rowI.get(i), this.matrixJ.get(j)));
+            }
+            newMat.add(newRow);
+
+            /* System.out.println("New Row: ");
+            for(int k = 0; k < newRow.size(); k++)
+            {
+                System.out.print(newRow.get(k) + " ");
+            }
+            System.out.println();*/
+        }
+
+        return newMat;
+
+        //TODO: figure out how to print the new rows to verify it works
+    }
+
     // Multiplies a row by a column
-    public int rowColMult(ArrayList<Integer> row, ArrayList<Integer> column)
+    private int rowColMult(ArrayList<Integer> row, ArrayList<Integer> column)
     {
         int sum = 0;
 
@@ -37,6 +74,7 @@ public class MatrixOps implements Runnable
             sum += (row.get(i) * column.get(i));
         }
 
+        // System.out.println("Element Op: " + sum);
         return sum;
     }
 }
