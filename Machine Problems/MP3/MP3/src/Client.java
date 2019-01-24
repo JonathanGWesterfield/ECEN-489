29974cee11e8fdb97a25715a5cd32fce91bf625a
Jonathan G. Westerfield <jgwesterfield@gmail.com>
2019-01-23 23:35:04 -0600

    Finished Client and almost finished server

--- Machine Problems/MP3/MP3/src/Client.java
+++ Machine Problems/MP3/MP3/src/Client.java
@@ -1,20 +1,64 @@
+// import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
+import java.lang.*;
 
 public class Client
 {
+    BufferedReader reader;
+    BufferedWriter writer;
+    OutputStreamWriter streamWriter;
+    Socket sock;
+    String hostName;
+    int portNumber;
+    String message;
+
     public static void main(String[] args)
     {
+        if(args.length != 3)
+        {
+            System.out.println("Usage: java Client <hostname> <portNumber> <message>");
+            System.exit(11);
+        }
+
         String hostName = args[0];
         int portNumber = Integer.parseInt(args[1]);
+        String message = args[2];
 
+        Client client = new Client(hostName, portNumber, message);
+        client.startClient();
+    }
+
+    public Client(String hostName, int portNumber, String message)
+    {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+        this.message = message;
+    }
+
+    public void startClient()
+    {
+        System.out.println("Starting Client!");
+        setUpNetworking();
+        sendMessage();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+    }
+
+    public void setUpNetworking()
+    {
         try
         {
-            Socket echoSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+            this.sock = new Socket(this.hostName, this.portNumber);
+            InputStreamReader streamReader = new InputStreamReader(this.sock.getInputStream());
+            this.reader = new BufferedReader(streamReader);
+            this.streamWriter = new OutputStreamWriter(this.sock.getOutputStream());
+            this.writer = new BufferedWriter(this.streamWriter);
+
+            System.out.println("Network Connection Established\n");
         }
         catch (IOException e)
         {
@@ -23,8 +67,40 @@ public class Client
         }
     }
 
-    public void createConn()
+    public void sendMessage()
     {
+        try
+        {
+            writer.write(message);
+            System.out.println("Message Sent!");
+            writer.flush();
+        }
+        catch (IOException e)
+        {
+            System.err.println("\nERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
 
     }
+
+    public class IncomingReader implements Runnable
+    {
+        public void run()
+        {
+            String message;
+            try
+            {
+                while((message = reader.readLine()) != null)
+                {
+                    System.out.println("Read: " + message);
+                }
+            }
+            catch (Exception e)
+            {
+                System.err.println("\nERROR: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- Machine Problems/MP3/MP3/src/Server.java
+++ Machine Problems/MP3/MP3/src/Server.java
@@ -1,2 +1,110 @@
-public class Server {
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Server
+{
+    private ServerSocket servSock;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    private ArrayList<PrintWriter> clientOutputStreams;
+
+    private int maxNumClients;
+    private int numClients;
+    private int portNum;
+    private String message;
+
+    public static void main(String[] args)
+    {
+        if(args.length != 3)
+        {
+            System.out.println("Usage: java Server <port number> <max # clients> <message>");
+            System.exit(11);
+        }
+
+        int portNum = Integer.parseInt(args[0]);
+        int maxNumClients = Integer.parseInt(args[1]);
+        String message = args[2];
+
+    }
+
+    public void Server(int portNum, int maxNumClients, String message)
+    {
+        this.portNum = portNum;
+        this.maxNumClients = maxNumClients;
+        this.message = message;
+        this.numClients = 0;
+    }
+
+    public void startServer()
+    {
+        try
+        {
+            this.servSock = new ServerSocket(this.portNum);
+            while (this.numClients <= this.maxNumClients)
+            {
+                Socket clientSocket = this.servSock.accept();
+                this.writer = new PrintWriter(clientSocket.getOutputStream(), true);
+                this.reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                this.clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+
+                System.out.println("Got a connection");
+                this.numClients += 1;
+            }
+
+            System.out.println("\nExceeded Max Number of Connections!!!");
+            System.out.println("Max Number of Connections Allowed: " + this.maxNumClients);
+            System.out.println("\nExiting!");
+        }
+        catch (IOException e)
+        {
+            System.err.println("\nERROR: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public class ClientHandler implements Runnable
+    {
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket)
+        {
+            try
+            {
+                this.sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                this.reader = new BufferedReader(isReader);
+            }
+            catch(Exception e)
+            {
+                System.err.println("\nERROR: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            String message;
+
+            try
+            {
+                while((message = this.reader.readLine()) != null)
+                {
+                    System.out.println("Server Read: " + message);
+                }
+            }
+            catch (IOException e)
+            {
+                System.err.println("\nERROR: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
 }
