580d06ce70af587dbb463fe4b6f75da1e090b4eb
Jonathan G. Westerfield <jgwesterfield@gmail.com>
2019-02-10 17:32:03 -0600

    Added new acivities to MP5

--- Machine Problems/MP5/MP5/app/src/main/java/com/machineproblem5/jonathanwesterfield/mp5/MainActivity.java
+++ Machine Problems/MP5/MP5/app/src/main/java/com/machineproblem5/jonathanwesterfield/mp5/MainActivity.java
@@ -1,7 +1,14 @@
 package com.machineproblem5.jonathanwesterfield.mp5;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onTextBtnClick(View view)
+    {
+
+    }
+
+    public boolean onMediaBtnClick(View view)
+    {
+
+    }
+
+    public boolean onImageBtnClick(View view)
+    {
+
+    }
+
+    public boolean checkExtStorage()
+    {
+        /* Checks if external storage is available to at least read */
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
+        {
+            return true;
+        }
+        return false;
     }
 }
